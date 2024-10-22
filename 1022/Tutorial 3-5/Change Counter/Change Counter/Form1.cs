ef1aa30fd79916edfd07cedb43527c61a8ad2b85
rry125 <A113221029@mail.shu.edu.tw>
2024-10-22 17:55:23 +0800

    1022

---
+++ 1022/Tutorial 3-5/Change Counter/Change Counter/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_Counter
+{
+    public partial class Form1 : Form
+    {
+        private decimal total;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void fiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total = total + 0.05m;
+            totalLabel.Text = total.ToString("c");
+        }
+
+        private void tenCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += 0.1m;
+            totalLabel.Text = total.ToString("c");
+        }
+
+        private void twentyFiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += 0.25m;
+            totalLabel.Text = total.ToString("c");
+        }
+
+        private void fiftyCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += 0.5m;
+            totalLabel.Text = total.ToString("c");
+        }
+
+        private void totalLabel_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
