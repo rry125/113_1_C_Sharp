94150bd9d3d47a0f28764035e7819cd997f3d38d
rry125 <A113221029@mail.shu.edu.tw>
2024-12-18 08:46:19 +0800

    hello

---
+++ 0924/hello/hello/Form1.cs
@@ -0,0 +1,16 @@
+namespace hello
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+
+        }
+    }
+}
